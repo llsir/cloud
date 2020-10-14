dbae535f45777c97f0b227b585d67da27b30214d
lsir <15188366128@163.com>
2020-10-14 11:34:10 +0800

    hhhh

--- test-client/src/main/java/com/boss/cloud/conf/StreamTest.java
+++ test-client/src/main/java/com/boss/cloud/conf/StreamTest.java
@@ -14,11 +14,14 @@ public class StreamTest {
         Student test1 = new Student(1, "test1", 20);
         Student test2 = new Student(2, "test3", 23);
         Student test3 = new Student(3, "test4", 10);
+        Student test4 = new Student(5, "test6", 230);
+        Student test5 = new Student(8, "test8", 220);
+        Student test7 = new Student(8, "test8", 220);
+        Student test8 = new Student(8, "test8", 220);
         List<Student> collect = Stream.of(test1, test2, test3, test4, test5).collect(Collectors.toList());
         List<Integer> list = collect.stream().map(Student::getId).collect(Collectors.toList());
         System.out.println(collect.toString());
         System.out.println(list.toString());
-        System.out.println(“1223”);
     }
 
 
